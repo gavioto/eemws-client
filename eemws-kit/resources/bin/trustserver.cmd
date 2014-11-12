efaa56d808662c04810aba75375db47e388ec407
Sistemas de Mercado <sistemas.mercado@ree.es>
2014-11-12 15:40:20 +0100

    Add new command line utility "trustserver"
    This program will add to the local truststore the signing and server certificates of the configured URLs

---
+++ eemws-kit/resources/bin/trustserver.cmd
@@ -0,0 +1,3 @@
+@echo off
+call commEnv.cmd
+@java %MEM_ARGS% %CONSOLE_LOG% es.ree.eemws.kit.cmd.trustserver.Main %*
---
+++ eemws-kit/resources/bin/trustserver.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+. ./commEnv.sh
+javaw $MEM_ARGS $CONSOLE_LOG es.ree.eemws.kit.cmd.trustserver.Main $*
