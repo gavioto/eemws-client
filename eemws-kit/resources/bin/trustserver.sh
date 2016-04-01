8098b0e4e0e2a2fb1658fe203fe66080c217cb15
Enrique Jiménez Raya <ejimenez@ree.es>
2016-04-01 12:50:51 +0200

    Change javaw to java. Javaw is not available on some linux systems

--- eemws-kit/resources/bin/trustserver.sh
+++ eemws-kit/resources/bin/trustserver.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 . ./commEnv.sh
-javaw $MEM_ARGS $JAVA_OPTIONS $CONSOLE_LOG es.ree.eemws.kit.cmd.trustserver.Main $*
+java $MEM_ARGS $JAVA_OPTIONS $CONSOLE_LOG es.ree.eemws.kit.cmd.trustserver.Main $*
