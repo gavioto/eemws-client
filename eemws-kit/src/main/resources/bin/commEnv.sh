c73b0674f625506a0ec04c3bda14a3530964f9b5
Enrique Jiménez <ejimenez@ree.es>
2016-06-29 10:46:04 +0200

    Added a new java option in the common script

--- eemws-kit/src/main/resources/bin/commEnv.sh
+++ eemws-kit/src/main/resources/bin/commEnv.sh
@@ -2,10 +2,10 @@
 CLASSPATH=""
 for f in "%INSTALL_PATH"/lib/*
 do
-  CLASSPATH="$CLASSPATH":$f
+  CLASSPATH="${CLASSPATH}":$f
 done
-CLASSPATH=$CLASSPATH:"%INSTALL_PATH/config"
-export CLASSPATH=$CLASSPATH
+CLASSPATH=${CLASSPATH}:"%INSTALL_PATH/config"
+export CLASSPATH=${CLASSPATH}
 
 export CONSOLE_LOG=-Djava.util.logging.config.file="%INSTALL_PATH/config/console-logging.properties"
 export FILE_LOG=-Djava.util.logging.config.file="%INSTALL_PATH/config/file-logging.properties"
@@ -17,7 +17,10 @@ export MEM_ARGS="-Xms32m -Xmx700m"
 export JAVA_OPTIONS=
 
 # Uncomment if you have to deal with systems that cannot handle SHA-2 algorithm
-#export JAVA_OPTIONS=$JAVA_OPTIONS -DUSE_LEGACY_SHA1
+#export JAVA_OPTIONS="${JAVA_OPTIONS} -DUSE_LEGACY_SHA1"
 
 # Uncomment if you want to send as binary compressed XML payload which size is greater than the specified number of characters (here 1000)
-#export JAVA_OPTIONS=$JAVA_OPTIONS -DXML_TO_BINARY_THRESHOLD_CHARS=1000
+#export JAVA_OPTIONS="${JAVA_OPTIONS} -DXML_TO_BINARY_THRESHOLD_CHARS=1000"
+
+# Uncomment if you face issues with server SNI miss configuration.
+#export JAVA_OPTIONS="${JAVA_OPTIONS} -Djsse.enableSNIExtension=false"
