994f67087a72fd060e6e4a83ef5f99aedb80b1f0
Enrique Jiménez Raya <ejimenez@ree.es>
2016-10-01 18:10:42 +0200

    Avoids get a file if it was already retrieved for the same configuration set

--- eemws-kit/src/main/java/es/ree/eemws/kit/folders/OutputTask.java
+++ eemws-kit/src/main/java/es/ree/eemws/kit/folders/OutputTask.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2015 Red Eléctrica de España, S.A.U.
+ * Copyright 2016 Red Eléctrica de España, S.A.U.
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published
@@ -39,17 +39,17 @@ import es.ree.eemws.core.utils.operations.list.ListOperationException;
 import es.ree.eemws.kit.common.Messages;
 
 /**
- * Execute a list + get loop to retrieve messages.
+ * Executes a list + get loop to retrieve messages.
  * 
  * @author Red Eléctrica de España, S.A.U.
- * @version 1.1 02/11/2015
+ * @version 1.2 15/09/2016
  * 
  */
 public final class OutputTask implements Runnable {
 
     /** Object which locks message for group working. */
     private LockHandler lh;
-        
+
     /** Log system. */
     private static final Logger LOGGER = Logger.getLogger(OutputTask.class.getName());
 
@@ -58,21 +58,21 @@ public final class OutputTask implements Runnable {
 
     /** File name extension for "xml". */
     private static final String FILE_NAME_EXTENSION_XML = "xml"; //$NON-NLS-1$
-    
+
     /** File name extension separator. */
     private static final String FILE_ELEMENTS_SEPARTOR = "."; //$NON-NLS-1$
 
     /** Headers values that identifies the file type. */
     @SuppressWarnings("nls")
     private static final String[] HEADER_VALUES = { "BZh91AY", "7z", "PDF", "PK", "PNG", "JFIF" };
-    
+
     /** File extensions according to the header value. */
     @SuppressWarnings("nls")
-    private static final String[] EXTENSION_VALUES = { "bz2", "7z", "pdf", "zip", "png", "jpg" }; 
-    
+    private static final String[] EXTENSION_VALUES = { "bz2", "7z", "pdf", "zip", "png", "jpg" };
+
     /** Reads up to 20 bytes in order to guess the proper file extension. */
     private static final int FIRST_BYTES_OF_MESSAGE = 20;
-    
+
     /** This task configuration set values. */
     private List<OutputConfigurationSet> ocs;
 
@@ -86,11 +86,14 @@ public final class OutputTask implements Runnable {
     private ListMessages list;
 
     /** Message get object. */
-    private GetMessage get; 
-    
+    private GetMessage get;
+
     /** This output task set of ids. */
     private String setIds;
-    
+
+    /** Last retrieved file. */
+    private RetrievedMessage lastRetrievedFile = null;
+
     /**
      * Constructor. Initializes parameters for detection thread.
      * @param lockHandler Lock Manager.
@@ -98,12 +101,12 @@ public final class OutputTask implements Runnable {
      * @param setIdss This output task set of ids.
      */
     public OutputTask(final LockHandler lockHandler, final List<OutputConfigurationSet> oc, final String setIdss) {
-        
+
         totalTypesToRetrieve = new ArrayList<>();
         boolean retrieveAllMessages = false;
-        for (OutputConfigurationSet o : oc) { 
+        for (OutputConfigurationSet o : oc) {
             LOGGER.info(o.toString());
-            
+
             if (!retrieveAllMessages) {
                 List<String> lstRetr = o.getMessagesTypesList();
                 if (lstRetr == null) {
@@ -114,19 +117,19 @@ public final class OutputTask implements Runnable {
                 }
             }
         }
-        
+
         URL endPoint = oc.get(0).getOutputUrlEndPoint();
-                
+
         list = new ListMessages();
         list.setEndPoint(endPoint);
-        
+
         get = new GetMessage();
         get.setEndPoint(endPoint);
-                        
+
         setIds = setIdss;
         lh = lockHandler;
-        ocs = oc;  
-        
+        ocs = oc;
+
     }
 
     /**
@@ -143,137 +146,187 @@ public final class OutputTask implements Runnable {
         if (lockFile) {
 
             try {
-                if (mle.getVersion() == null) {
-                    LOGGER.info(Messages.getString("MF_RETRIEVING_MESSAGE_WO_VERSION", setIds, codeStr, mle.getMessageIdentification())); //$NON-NLS-1$
-                } else {
-                    LOGGER.info(Messages.getString("MF_RETRIEVING_MESSAGE", setIds, codeStr, mle.getMessageIdentification(), mle.getVersion())); //$NON-NLS-1$
-                }
-
-                RetrievedMessage response = get.get(code);
-
-                if (mle.getVersion() == null) {
-                    LOGGER.info(Messages.getString("MF_RETRIEVED_MESSAGE_WO_VERSION", setIds, codeStr, mle.getMessageIdentification())); //$NON-NLS-1$
-                } else {
-                    LOGGER.info(Messages.getString("MF_RETRIEVED_MESSAGE", setIds, codeStr, mle.getMessageIdentification(), mle.getVersion())); //$NON-NLS-1$
-                }
+                
+                lastRetrievedFile = null;
 
                 for (OutputConfigurationSet oc : ocs) {
                     List<String> type = oc.getMessagesTypesList();
                     if (type == null || type.contains(mle.getType())) {
-                        saveFile(mle, response, oc.getOutputFolder(), oc.getFileNameExtension(), oc.getProgramCmdLine(), oc.getIndex());
-                    }                    
+                        saveFile(mle, oc);
+                    }
                 }
-                
+
             } catch (GetOperationException e) {
                 if (mle.getVersion() == null) {
                     LOGGER.log(Level.SEVERE, Messages.getString("MF_UNABLE_TO_GET_WO_VERSION", setIds, String.valueOf(code), mle.getMessageIdentification()), e); //$NON-NLS-1$
                 } else {
                     LOGGER.log(Level.SEVERE, Messages.getString("MF_UNABLE_TO_GET", setIds, String.valueOf(code), //$NON-NLS-1$
-                            mle.getMessageIdentification(), mle.getVersion()), e); 
+                            mle.getMessageIdentification(), mle.getVersion()), e);
                 }
             } catch (IOException e) {
                 if (mle.getVersion() == null) {
                     LOGGER.log(Level.SEVERE, Messages.getString("MF_UNABLE_TO_SAVE_WO_VERSION", setIds, String.valueOf(code), mle.getMessageIdentification()), e); //$NON-NLS-1$
                 } else {
                     LOGGER.log(Level.SEVERE, Messages.getString("MF_UNABLE_TO_SAVE", setIds, String.valueOf(code), //$NON-NLS-1$
-                            mle.getMessageIdentification(), mle.getVersion()), e); 
+                            mle.getMessageIdentification(), mle.getVersion()), e);
                 }
             } finally {
                 lh.releaseLock(codeStr);
+                
+                /* Avoid holding this (probably) huge value */
+                lastRetrievedFile = null;
             }
         }
     }
 
     /**
-     * Saves the retrieved message and (optionally) executes a program.
-     * @param mle Retrieved message information.
-     * @param response Retrieved message.
-     * @param outPath Path to save.
-     * @param fExtension File extension to be added to the file name.
-     * @param cmdLine Program to execute. <code>null</code> if no program have to be executed.
-     * @param setIndex The index of the output set.
-     * @throws IOException If the message cannot be saved or if the provided command line produces error.
+     * Gets the message given its <code>MessageListEntry</code>.
+     * The method wont call the get operation if the file was already retrieved.
+     * @param mle MessageListEntry with the information about the file to be retrieved.
+     * @return The message retrieved according the given MessageListEntry parameter. <code>null</code> is never returned.
+     * @throws GetOperationException If the message cannot be retrieved.
      */
-    private void saveFile(final MessageListEntry mle, final RetrievedMessage response, final String outPath, 
-            final String fExtension, final String cmdLine, final int setIndex) throws IOException {
+    private RetrievedMessage retrieveFile(final MessageListEntry mle) throws GetOperationException {
+
+        if (lastRetrievedFile == null) {
+
+            long code = mle.getCode().longValue();
+            String codeStr = String.valueOf(code);
+
+            if (mle.getVersion() == null) {
+                LOGGER.info(Messages.getString("MF_RETRIEVING_MESSAGE_WO_VERSION", setIds, codeStr, mle.getMessageIdentification())); //$NON-NLS-1$
+            } else {
+                LOGGER.info(Messages.getString("MF_RETRIEVING_MESSAGE", setIds, codeStr, mle.getMessageIdentification(), mle.getVersion())); //$NON-NLS-1$
+            }
+
+            lastRetrievedFile = get.get(code);
+
+            if (mle.getVersion() == null) {
+                LOGGER.info(Messages.getString("MF_RETRIEVED_MESSAGE_WO_VERSION", setIds, codeStr, mle.getMessageIdentification())); //$NON-NLS-1$
+            } else {
+                LOGGER.info(Messages.getString("MF_RETRIEVED_MESSAGE", setIds, codeStr, mle.getMessageIdentification(), mle.getVersion())); //$NON-NLS-1$
+            }
+        }
         
-        String fileName = getFileName(mle, response, fExtension);
-        String abosoluteFileName = outPath + File.separator + fileName;
+        return lastRetrievedFile;
+
+    }
+
+    /**
+     * Saves the current message and (optionally) executes a program.
+     * @param mle Retrieved message information.
+     * @param oc Configuration to be used in order to save the file. 
+     * @throws IOException If the message cannot be saved or if the provided command line produces error.
+     * @throws GetOperationException  If the message cannot be retrieved.
+     */
+    private void saveFile(final MessageListEntry mle, final OutputConfigurationSet oc) throws IOException, GetOperationException {
+
+        String fileName = calculateFileName(mle, oc.getFileNameExtension());
+
+        String abosoluteFileName= oc.getOutputFolder() + File.separator + fileName;
 
         if (FileUtil.exists(abosoluteFileName)) {
 
-            LOGGER.info(Messages.getString("MF_RETRIEVED_MESSAGE_ALREADY_EXISTS", setIndex, abosoluteFileName)); //$NON-NLS-1$
+            LOGGER.info(Messages.getString("MF_RETRIEVED_MESSAGE_ALREADY_EXISTS", oc.getIndex(), abosoluteFileName)); //$NON-NLS-1$
 
         } else {
-
+ 
+            RetrievedMessage message = retrieveFile(mle);
+  
             /*
-             * Avoid "broken files" in case of anormal program termination. First write into a temporaly file
-             * then rename it.
+             * Avoid "broken files" in case of anormal program termination. First write into a temporaly file then
+             * rename it.
              */
             File tmpFile;
-            tmpFile = File.createTempFile(TMP_PREFIX, null, new File(outPath));
-            if (response.isBinary()) {
-                FileUtil.write(tmpFile.getAbsolutePath(), response.getBinaryPayload());
+            tmpFile = File.createTempFile(TMP_PREFIX, null, new File(oc.getOutputFolder()));
+            if (message.isBinary()) {
+                FileUtil.write(tmpFile.getAbsolutePath(), message.getBinaryPayload());
             } else {
-                FileUtil.writeUTF8(tmpFile.getAbsolutePath(), response.getPrettyPayload());
+                FileUtil.writeUTF8(tmpFile.getAbsolutePath(), message.getPrettyPayload()); 
             }
 
             File file = new File(abosoluteFileName);
             tmpFile.renameTo(file);
-            
-            ProgramExecutor.execute(cmdLine, file,  null,  mle.getType());            
+
+            ProgramExecutor.execute(oc.getProgramCmdLine(), file, null, mle.getType());
         }
-    }
+
+    } 
 
     /**
-     * Returns the file name  according to the configuration and the retrieved message.
-     * @param mle message entry which file name will be returned.
-     * @param response Retrieved message.
-     * @param fExtension File extension to be added.
-     * @return Proper file name. 
+     *  Gets the file name extension according to the given extension configuration value.
+     *  The extension is obtained as following:
+     *  <li> AUTO (for binary): fileName + ["." + calculatedExtension"] </li>
+     *  <li> AUTO (non binary): fileName + ["." + version] + ".xml" </li>
+     *  <li> NONE: messageId + ["." + version] </li>
+     *  <li> XXXX: messageId + ["." + version] + ".XXXX </li>
+     *  
+     *  NOTE: If the retrieved binary file has an unsupported extension the calculated file name won't have extension.
+     *  NOTE: AUTO forces file retrieving in order to know the proper extension according to the file's content.
+     *  
+     *  @param mle Current message list entry.
+     *  @param fExtension Extension configuration value.
+     *  @throws GetOperationException if file cannot be retrieved.
      */
-    private String getFileName(final MessageListEntry mle, final RetrievedMessage response, final String fExtension) {
+    private String calculateFileName(final MessageListEntry mle, final String fExtension) throws GetOperationException {
 
-        StringBuilder retValue = new StringBuilder();
-
-        if (response.isBinary()) {
-            retValue.append(response.getFileName());
-        } else {
-            retValue.append(mle.getMessageIdentification());
-
-            if (mle.getVersion() != null) {
-                retValue.append(FILE_ELEMENTS_SEPARTOR);
-                retValue.append(mle.getVersion());
-            }
-        }
-
+        StringBuilder extStr = new StringBuilder();
+        
+        
         if (fExtension.equalsIgnoreCase(OutputConfigurationSet.FILE_NAME_EXTENSION_AUTO)) {
-            if (response.isBinary()) {
-                byte[] b = response.getBinaryPayload();
+
+            /* Extension == AUTO and the file was not yet retrieved -> get the file */
+            RetrievedMessage message = retrieveFile(mle);
+
+            if (message.isBinary()) {
+                            
+                extStr.append(message.getFileName());
+                               
+                byte[] b = message.getBinaryPayload();
                 if (b.length > FIRST_BYTES_OF_MESSAGE) {
-                    String headerValue = new String(response.getBinaryPayload(), 0, FIRST_BYTES_OF_MESSAGE);
+                    String headerValue = new String(message.getBinaryPayload(), 0, FIRST_BYTES_OF_MESSAGE);
                     boolean found = false;
                     for (int cont = 0; cont < HEADER_VALUES.length && !found; cont++) {
                         if (headerValue.indexOf(HEADER_VALUES[cont]) != -1) {
-                            retValue.append(FILE_ELEMENTS_SEPARTOR);
-                            retValue.append(EXTENSION_VALUES[cont]);
+                            extStr.append(FILE_ELEMENTS_SEPARTOR);
+                            extStr.append(EXTENSION_VALUES[cont]);
                             found = true;
                         }
                     }
                 }
 
             } else {
-                retValue.append(FILE_ELEMENTS_SEPARTOR);
-                retValue.append(FILE_NAME_EXTENSION_XML);
+            
+                extStr.append(mle.getMessageIdentification());
+                
+                if (mle.getVersion() != null) {
+                    extStr.append(FILE_ELEMENTS_SEPARTOR);
+                    extStr.append(mle.getVersion());
+                    extStr.append(FILE_ELEMENTS_SEPARTOR);
+                    extStr.append(FILE_NAME_EXTENSION_XML);
+                }
             }
-        } else if (!fExtension.equalsIgnoreCase(OutputConfigurationSet.FILE_NAME_EXTENSION_NONE)) {
-            retValue.append(FILE_ELEMENTS_SEPARTOR);
-            retValue.append(fExtension);
-        }
 
-        return retValue.toString();
+        } else {
+            
+            extStr.append(mle.getMessageIdentification());
+                        
+            if (mle.getVersion() != null) {
+                extStr.append(FILE_ELEMENTS_SEPARTOR);
+                extStr.append(mle.getVersion());
+            }
+
+            if (!fExtension.equalsIgnoreCase(OutputConfigurationSet.FILE_NAME_EXTENSION_NONE)) {
+                extStr.append(FILE_ELEMENTS_SEPARTOR);
+                extStr.append(fExtension);
+             }
+        }
+        
+        
+        return extStr.toString();
     }
 
+   
     /**
      * Gets a message list using the last list code.
      * @return A message list. <code>null</code> if the client cannot connect with the server.
